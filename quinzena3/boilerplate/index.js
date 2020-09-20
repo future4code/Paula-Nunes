b464b4953339d48e8a76b0d0694922aeb7fd624c
paula karine brito nunes <osmitis@gmail.com>
2020-09-20 10:34:54 -0300

    exercicio 10

--- quinzena3/boilerplate/index.js
+++ quinzena3/boilerplate/index.js
@@ -20,3 +20,13 @@ resultado = !resultado && (bool1 || bool1)
 console.log("c. ", resultado)
 
 console.log("e. ", typeof resultado)
+
+/*Como você faria para dar saudações diferentes para as pessoas, dependendo do gênero com a qual se identificam? 
+Ex.: Feminino → Bem-vinda; Masculino → Bem-vindo*/
+
+/*Como você faria para identificar se um número é par ou ímpar; e escrever no terminal:
+ "este número é par" ou "este número é ímpar"?*/
+
+/*Dados dois números a e b, como você faria para escrever as mensagens: "a é maior do que b"; 
+"b é maior do que a" ou "b é igual a a" (dependendo dos valores de a e b)?
+Escolhe um ficheiro ou arrasta para aqui Limite de tamanho: 10MB*/
